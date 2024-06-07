e8b366573e7eb992ad9dafdabf3e55c263f5a16a
Nathan Martinez <nmart110@calpoly.edu>
2024-06-07 02:32:16 -0700

    clips stuff parsing

--- backend/api/rule_engine/clipsScript.py
+++ backend/api/rule_engine/clipsScript.py
@@ -1,8 +1,37 @@
 import clips
 
 # Cost
-# Capicty
+# Capacity
 # Availability
 # Ratings
 # Category
+
+deftemplateEvent = """
+(deftemplate Event
+  (slot id)
+  (slot type)
+  (slot date)
+  (slot location)
+  (slot guest-capacity)
+  (slot budget))
+"""
+
+deftemplateVendor = """
+(deftemplate vendor
+  (slot id)
+  (slot name)
+  (slot location)
+  (slot service-type)
+  (multislot availability)
+  (slot cost)
+  (slot capacity)
+  (slot customer-ratings))
+"""
+
+deftemplateMatch = """
+(deftemplate match
+  (slot event-id)
+  (slot vendor-id)
+  (slot service-type))
+"""
 
